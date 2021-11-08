8e7bf9992b63bef9359f7b3318177c5594c479e4
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-11-08 09:49:41 +0600

    Create Palindrome.py

---
+++ Random-Problems/Palindrome.py
@@ -0,0 +1,20 @@
+
+def check_Palindrome(strng):
+    strng1 = ''
+    strng2 = ''
+    for i in strng:
+        if i == ' ':
+            strng1 += i
+        else:
+            strng2 += i
+
+    new_strng = ''
+    new_strng = strng2[-1::-1]
+
+    if new_strng == strng2:
+        print("This is a palindrome")
+    else:
+        print('This is not a palindrome')
+
+
+check_Palindrome(input("enter a string: "))
